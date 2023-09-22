1094e9fc470a271bded3f4bdac4713fae9b5016b
pkgalaxy <pkgalaxyworld@gmail.com>
2023-09-22 17:34:26 +0530

    Updated 12

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -117,8 +117,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'resume/static'),
+
 
 
 
@@ -127,6 +130,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,12 @@
 
 from django.contrib import admin      # from django framework >>> contrib module >>> importing  admin site / class
 from django.urls import path, include    # from django framework >>> urls module >>>  path and include function
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('resume.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
